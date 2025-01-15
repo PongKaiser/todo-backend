c15acd116f091fb004ba7595493a9a845fe79dc6
Kaiser <kaiser@Kaisers-MacBook-Pro.local>
2025-01-15 11:08:21 -0800

    No authorization

--- src/middleware/authCheck.js
+++ src/middleware/authCheck.js
@@ -12,13 +12,13 @@ verifyToken = (req, res, next) => {
     return res.status(403).send({ message: "No token provided!" });
   }
 
-  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
-    // if (err) {
-    //   return res.status(401).send({ message: "Unauthorized!" });
-    // }
-    req.userId = decoded.id;
-    next();
-  });
+  // jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+  //   if (err) {
+  //     return res.status(401).send({ message: "Unauthorized!" });
+  //   }
+  //   req.userId = decoded.id;
+  //   next();
+  // });
 };
 
 const authCheck = {
